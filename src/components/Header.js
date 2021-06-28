49b21d8aef0656346aec2e7a7eb9be6274b11fe1
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-06-27 17:29:22 -0700

    end of weekend homework. Imgae included

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import ticketsImage from "./../img/tickets.jpeg";
 
 function Header(){
   return (
+    <React.Fragment>
     <h1>Help Queue</h1>
+    <img src={ticketsImage} alt="an image of a corner of a building with the sign reading 'Tickets'" />
+    </React.Fragment>
   );
 }
 
