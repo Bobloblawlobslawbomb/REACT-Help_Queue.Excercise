d2005e8e6cfa350ac19f7e9fc5b3f6392cb62ea7
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-06-30 00:09:19 -0700

    some styling --NewTicketForm styling wonky

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,14 @@ import React from "react";
 import ticketsImage from "./../img/tickets.jpeg";
 
 
-function Header(){
+function Header(props){
+  const myFunkyStles = {
+    height: '40vh',
+    textAlign: 'center'
+  }
 
   return (
-      <div>
+      <div style={myFunkyStles}>
         <h1>Help Queue</h1>
         <img src={ticketsImage} alt="a corner of a building with the sign reading 'Tickets'" />
       </div>
--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -3,6 +3,14 @@ import { v4 } from 'uuid';
 import PropTypes from "prop-types";
 
 function NewTicketForm(props) {
+
+  // const myStyles = {
+  //   width: '70%',
+  //   padding: '10',
+  //   margin: 'auto',
+  //   textAlign: 'center'
+  // }
+
   function handleNewTicketFormSubmission(e) {
     e.preventDefault();
     props.onNewTicketCreation({names: e.target.names.value, location: e.target.location.value, issue: e.target.issue.value, id: v4()});
@@ -10,21 +18,26 @@ function NewTicketForm(props) {
 
   return (
     <React.Fragment>
+    {/* <div style={myStyles}> */}
       <form onSubmit={handleNewTicketFormSubmission}>
         <input
           type='text'
           name='names'
           placeholder='Pair Names' />
+          <hr/>
         <input
           type='text'
           name='location'
           placeholder='Location' />
+          <hr/>
         <textarea
           name='issue'
           placeholder='Describe your issue.' />
+          <hr/>
         <button type='submit'>Help!</button>
-        
+        <hr/>
       </form>
+    {/* </div> */}
     </React.Fragment>
   );
 };
